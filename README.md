bbdda3a29b9f07fb60aac1ceb6ccf0ab1a7ee55a
croxis <croxis@yahoo.com>
2016-08-28 11:14:10 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# planetrise
+# Module planetrise
 Calculates and display the rise and set times of the planets for Magic Mirror 2
+
+This module is powered with Don Cross's javascript library [astronomy.js](http://cosinekitty.com).
+## Using the module
+
+To use this module, add it to the modules array in the `config/config.js` file:
+````javascript
+modules: [
+	{
+		module: 'planetrise',
+		position: 'top_right',	// This can be any of the regions.
+		header: 'PLanet Rise',
+        config: {  // Place the latitude and longitude of your mirror
+            latitude: 45.5,
+            longitude: -122.38,
+            // A dictiory of the bodies and unicode character for the symbol
+            // This is the default and does not need to be listed.
+            // A full list of bodies can be seen on line 1359 in astronomy.js
+            // Note: Trying to find the rise time of Earth will crash the Module
+            bodies: {'Sun': '☉',
+                    'Moon': '☽',
+                    'Mercury': '☿',
+                    'Venus': '♀',
+                    'Mars': '♂',
+                    'Jupiter': '♃',
+                    'Saturn': '♄',
+            }
+        }
+	}
+]
+````
---
+++ planetrise.js
@@ -0,0 +1,99 @@
+// Helper functions from http://cosinekitty.com/solar_system.html
+var BriefDayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    function BriefTimeString (date)
+    {
+        if (date == null) {
+            return "";
+        } else {
+            var h = date.getHours();
+            h = ((h < 10) ? "0" : "") + h.toString();
+            var m = date.getMinutes();
+            m = ((m < 10) ? "0" : "") + m.toString();
+            var s = date.getSeconds();
+            s = ((s < 10) ? "0" : "") + s.toString();
+            return BriefDayOfWeek[date.getDay()] + " " + h + ":" + m + ":" + s;
+        }
+    }
+
+
+function BriefDayValueString (day)
+    {
+        if (day == null) {
+            return "";
+        } else {
+            return BriefTimeString (Astronomy.DayValueToDate (day));
+        }
+    }
+
+Module.register("planetrise", {
+    defaults: {
+        latitude: 34.2,
+        longitude: -118.1,
+        //bodies: ['Sun', 'Moon', 'Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn']
+        bodies: {'Sun': '☉',
+                'Moon': '☽',
+            'Mercury': '☿',
+            'Venus': '♀',
+            'Mars': '♂',
+            'Jupiter': '♃',
+            'Saturn': '♄',
+        }
+    },
+    // Define start sequence.
+	start: function() {
+		Log.info("Starting module: " + this.name);
+
+		// Schedule update interval.
+		var self = this;
+		setInterval(function() {
+			self.updateDom();
+		}, 1000*60);
+    },
+    // Override dom generator.
+    getDom: function() {
+        latitude = this.config.latitude;
+        longitude = this.config.longitude;
+
+        var wrapper = document.createElement("table");
+        wrapper.className = "small";
+
+        var AstroDateTime = new Date();
+        var day = Astronomy.DayValue (AstroDateTime);
+        var location = new GeographicCoordinates(longitude, latitude, 0);
+
+        for (var i in Astronomy.Body) {
+            //AddRowForCelestialBody (Astronomy.Body[i], day);
+            if (Object.keys(this.config.bodies).indexOf(Astronomy.Body[i].Name) >= 0){
+                var planetWrapper = document.createElement("tr");
+                planetWrapper.className = "normal";
+                var symbolWrapper = document.createElement("td");
+                symbolWrapper.className = "symbol";
+                //If fontio ever supports the full set
+                //var symbol =  document.createElement("span");
+                //symbol.className = "fa fa-" + this.config.bodies[Astronomy.Body[i].Name];
+                //symbolWrapper.appendChild(symbol);
+                symbolWrapper.innerHTML = '<center>' + this.config.bodies[Astronomy.Body[i].Name] + '</center>';
+                planetWrapper.appendChild(symbolWrapper);
+                var titleWrapper = document.createElement("td");
+                titleWrapper.innerHTML = Astronomy.Body[i].Name;
+                titleWrapper.className = "title bright";
+                planetWrapper.appendChild(titleWrapper);
+                var riseWrapper = document.createElement("td");
+                riseWrapper.className = "time light";
+                riseWrapper.innerHTML = BriefDayValueString(Astronomy.NextRiseTime(Astronomy.Body[i], day, location));
+                planetWrapper.appendChild(riseWrapper);
+                var setWrapper = document.createElement("td");
+                setWrapper.className = "time light";
+                setWrapper.innerHTML = BriefDayValueString(Astronomy.NextSetTime(Astronomy.Body[i], day, location));
+                planetWrapper.appendChild(setWrapper);
+                //planetWrapper.innerHTML = Object.keys(Astronomy.Body[i]).toString();
+                wrapper.appendChild(planetWrapper);
+            }
+        }
+        //wrapper.innerHTML = make_text(sun_elevation, next, julian_date);
+        return wrapper;
+    },
+    getScripts: function() {
+        return['astronomy.js']
+    }
+});
